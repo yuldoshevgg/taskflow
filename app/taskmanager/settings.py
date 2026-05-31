5eef3f02f770aca737bd790f1dc2fb802d9ae1b3
Gayrat Yuldoshev <devinfinitum0@gmail.com>
2026-05-31 13:29:03 +0500

    Add initial migrations and fix STORAGES deprecation warning for Django 5.x compat

--- app/taskmanager/settings.py
+++ app/taskmanager/settings.py
@@ -77,7 +77,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
