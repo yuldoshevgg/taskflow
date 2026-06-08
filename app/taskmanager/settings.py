112e2a3431652fd5507959edb0324fd1cbea27bc
Gayrat Yuldoshev <devinfinitum0@gmail.com>
2026-06-08 14:43:01 +0500

    Add CSRF_TRUSTED_ORIGINS setting for Cloudflare proxy

--- app/taskmanager/settings.py
+++ app/taskmanager/settings.py
@@ -88,3 +88,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
+
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='').split(',')
